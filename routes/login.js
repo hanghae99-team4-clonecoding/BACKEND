7c4bcbde15d862910de4105de911cf5eaba45912
JJooonji <korewa9@naver.com>
2022-08-20 01:08:18 +0900

    로그인 코드 1차 작성

---
+++ routes/login.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  // try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where : {email, password} })
+
+    if(!user) {
+        res.status(400).send({
+            error: "이메일 또는 비밀번호가 일치하지 않습니다."
+        });
+    }
+
+    const token = jwt.sign({ userId: user.userId }, "clone4-secret-key"); 
+    console.log(token, "토큰확인")
+
+    res.status(200).json({ token, email, message:"로그인이 완료되었습니다." })
+    console.log("로그인이 완료되었습니다.")
+    // } catch(error) {
+    //     res.status(401).send({
+    //         error:"로그인 후 사용하세요."
+    //     });
+    // }
+})
+
+module.exports = router;
