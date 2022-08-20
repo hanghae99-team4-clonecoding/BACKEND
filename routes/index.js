e0d983f7e04edfa86ca0d791a7004c207c0337c8
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-08-20 15:29:52 +0900

    signup router 추가

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const auth = require("../middlewares/auth-middleware");
 //const userRouter = require("./users");
 const loginRouter = require("./login");
 const postRouter = require("./posts");
+const signupRouter = require("./signup");
 
 //router.use("/", userRouter);
 router.use("/", loginRouter);
+router.use("/signup", signupRouter);
 router.use("/post", auth, postRouter);
 
 module.exports = router;
