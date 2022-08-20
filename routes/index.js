af381801c0a483fb46135b9cf3140af5295f5b40
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-08-20 15:00:47 +0900

    미들웨어 추가

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middlewares/auth-middlewares");
 
-// const userRouter = require("./users");
+//const userRouter = require("./users");
 const loginRouter = require("./login");
-const signupRouter = require("./signup");
-// const postRouter = require("./posts")
+const postRouter = require("./posts");
 
-
-// router.use("/", userRouter);
+//router.use("/", userRouter);
 router.use("/", loginRouter);
-router.use("/signup", signupRouter);
-// router.use("/post", postRouter);
+router.use("/post", auth, postRouter);
 
 module.exports = router;
