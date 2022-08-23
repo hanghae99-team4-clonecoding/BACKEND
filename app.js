9531ad08c3a6f9927aed7523a380133b3fbf2376
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-08-24 00:52:54 +0900

    loging 구현

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const { sequelize } = require("./models");
 const cors = require("cors");
 const router = require("./routes");
+const morgan = require("morgan");
+const accessLogStream = require("./utils/log");
 
 require("dotenv").config(); // npm i dotenv
 const port = process.env.PORT; //process.env는 내장 함수로 .env파일의 PORT란 변수를 불러와줌.
@@ -11,11 +13,11 @@ const port = process.env.PORT; //process.env는 내장 함수로 .env파일의 P
 const corsOption = {
   origin: true,
   credentials: true,
-}
+};
 
 //db 생성 부분
 sequelize
-  .sync({ force : false }) //db reset할 일 있으면 true 변경
+  .sync({ force: false }) //db reset할 일 있으면 true 변경
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
@@ -29,6 +31,12 @@ const requestMiddleWare = (req, res, next) => {
 };
 
 app.use(cors(corsOption));
+
+if (process.env.NODE_ENV === "product") {
+  app.use(morgan("combined", { stream: accessLogStream }));
+} else {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(requestMiddleWare);
--- routes/likes.js
+++ routes/likes.js
@@ -5,35 +5,28 @@ const { Like, Post } = require("../models");
 const Joi = require("joi");
 const { Op } = require("sequelize");
 
-
-
 router.post("/:postId", async (req, res) => {
   try {
     const { postId } = req.params;
     const user = res.locals.user;
 
-    existedLike= await Like.findOne({where : {userId : user.userId, postId}});
-    if (!existedLike){
-        Like.create({userId : user.userId, postId}).then(()=>{
-            Like.count({where:{postId},}).then((count)=>{
-                Post.findOne({where : {postId}}).then((existsPost)=>{
-                    existsPost.likeCount=count;
-                    existsPost.save();
-                    return res.status(200).json({message : "좋아요 클릭!"});
-                });
-            });
-        });
-    }else {
-        existedLike.destroy().then(()=>{
-            Like.count({where:{postId},}).then((count)=>{
-                Post.findOne({where: {postId}}).then((existsPost)=>{
-                    console.log("count가 있는지? : ",count);
-                    existsPost.likeCount=count;
-                    existsPost.save();
-                    return res.status(200).json({message : "좋아요 지우기!"});
-                });
-            });
-        });
+    existedLike = await Like.findOne({
+      where: { userId: user.userId, postId },
+    });
+    if (!existedLike) {
+      await Like.create({ userId: user.userId, postId });
+      const count = await Like.count({ where: { postId } });
+      const existsPost = await Post.findOne({ where: { postId } });
+      existsPost.likeCount = count;
+      existsPost.save();
+      return res.status(200).json({ message: "좋아요 클릭!" });
+    } else {
+      await existedLike.destroy();
+      const count = await Like.count({ where: { postId } });
+      const existsPost = await Post.findOne({ where: { postId } });
+      existsPost.likeCount = count;
+      existsPost.save();
+      return res.status(200).json({ message: "좋아요 지우기!" });
     }
 
   } catch (error) {
@@ -41,6 +34,5 @@ router.post("/:postId", async (req, res) => {
     return res.status(400).json({ error: "좋아요 클릭/삭제에 실패했습니다." });
   }
 });
-
 
 module.exports = router;
